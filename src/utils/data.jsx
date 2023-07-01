92cf05ea534ec39fdd15915eea261e4814d4f1b9
frank-Mwangi <kungofrank@gmail.com>
2023-07-01 09:48:09 +0300

    Edits on our services page

--- src/utils/data.jsx
+++ src/utils/data.jsx
@@ -1,5 +1,9 @@
 import { nanoid } from "nanoid";
-import { BsFillBriefcaseFill, BsWrenchAdjustableCircleFill, BsCashCoin } from "react-icons/bs"
+import {
+  BsFillBriefcaseFill,
+  BsWrenchAdjustableCircleFill,
+  BsCashCoin,
+} from "react-icons/bs";
 const links = [
   {
     id: nanoid(),
@@ -21,22 +25,23 @@ const services = [
   {
     id: nanoid(),
     icon: <BsWrenchAdjustableCircleFill />,
-    title: 'maintainance',
-    info: "We have a team of skilled technicians with vast knowledge of the products we sell therefore we can ensure that any malfunction that occurs to any of our products will be dealt with swiftly." },
+    title: "maintainance",
+    info: "We have a team of skilled technicians with a vast knowledge of the products we sell. Therefore, we are more than capable of addressing any malfunction in any of our products.",
+  },
   {
     id: nanoid(),
     icon: <BsFillBriefcaseFill />,
-    title: 'consultancy',
-    info: "If you have a startup business and you are having trouble setting up your tech infrastucture, A2Z will help you navigate through choosing the right products tailored to your company." 
+    title: "consultancy",
+    info: "Do you have a startup business and are having trouble setting up your tech infrastucture? A2Z is here to help you choose the ideal products especially tailored to your company's needs.",
   },
   {
     id: nanoid(),
     icon: <BsCashCoin />,
-    title: 'sales',
-    info: "While we prioritize quality, we also understand the significance of affordability. We believe that everyone should have access to superior products at reasonable prices."
-  }
-]
-export { links, services }
+    title: "sales",
+    info: "While we prioritize quality, we also understand the significance of affordability. We believe that everyone should have access to superior products at reasonable prices.",
+  },
+];
+export { links, services };
 
-export const products_url = '/.netlify/functions/products'
-export const single_product_url = `/.netlify/functions/single-product?id=`
+export const products_url = "/.netlify/functions/products";
+export const single_product_url = `/.netlify/functions/single-product?id=`;
